fc69521779f80acaca46983854b5f560e970d295
zluo16 <zluo@alumni.berklee.edu>
2017-08-06 18:57:08 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  handleOnClick = () => {
+    this.props.store.dispatch({
+      type: 'INCREASE_COUNT'
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        App component
+        <button onClick={this.handleOnClick}>
+          Click
+        </button>
+        <p>{this.props.store.getState().items.length}</p>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import shoppingListItemReducer from './reducers/shoppingListItemReducer';
 import App from './App';
 import './index.css';
 
+const store = createStore(shoppingListItemReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App store={store} />
+  </Provider>,
   document.getElementById('root')
 );
